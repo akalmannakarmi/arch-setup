861e3a9e3c6e4450c144cf775f72b0f258198871
Akal Nakarmi <akalnakarmi1@gmail.com>
2025-10-03 17:57:31 +0545

    fix post setup path

--- deps/post-setup.sh
+++ deps/post-setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-===== Customize Hyde project =====
+# ===== Customize Hyde project =====
 scriptDir="$HOME/.local/lib/hyde"
 "${scriptDir}/wallbashtoggle.sh" 2
 "${scriptDir}/theme.switch.sh" -s "Tokyo Night"
 
 
 # ===== Install and Setup Conda =====
-source ./deps/install-conda.sh
+source /home/$USER/deps/install-conda.sh
