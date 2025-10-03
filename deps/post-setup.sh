e3820b47a07f809fac0b5c141575f216444903e4
Akal Nakarmi <akalnakarmi1@gmail.com>
2025-10-03 22:55:38 +0545

    fix post setup

--- deps/post-setup.sh
+++ deps/post-setup.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -e
 
-# File that marks the script has already run
-FLAG="$HOME/arch-setup/.post-setup-done"
-
 # Exit if script has already run
-if [ -f "$FLAG" ]; then
+if [ -f "$HOME/arch-setup/.post-setup-done" ]; then
     exit 0
 fi
 
 # ===== Customize Hyde project =====
 scriptDir="$HOME/.local/lib/hyde"
-source "${scriptDir}/wallbashtoggle.sh" 2
-source "${scriptDir}/theme.switch.sh" -s "Tokyo Night"
+"${scriptDir}/wallbashtoggle.sh" 2
+"${scriptDir}/theme.switch.sh" -s "Tokyo Night"
 
 
 # ===== Install and Setup Conda =====
